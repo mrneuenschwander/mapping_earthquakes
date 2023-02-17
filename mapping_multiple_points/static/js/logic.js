fb0bc3bdf5130380c3ce2c2007cdd36088666bf5
mrneuenschwander <116296092+mrneuenschwander@users.noreply.github.com>
2023-02-16 17:26:09 -0700

    mapping of multiple points
    - markers based on a city array
    - circles based on the same
    - added popups within the forEach loop performed on the array

--- mapping_multiple_points/static/js/logic.js
+++ mapping_multiple_points/static/js/logic.js
@@ -4,23 +4,41 @@ console.log("working");
 // Create the map object with a center and zoom level.
 let map = L.map('mapid').setView([40.7, -94.5], 4);
 
-// add points
-//  Add a marker to the map for Los Angeles, California. This generates a point.
-let marker = L.marker([34.0522, -118.2437]).addTo(map);
+// reference the cities objects in the cities.js file
+let cityData = cities
 
-//  add a circle marker
-L.circle([34.0522, -118.2437], {
-    radius: 100
- }).addTo(map);
+// // iterate through the cities array and add its marker to the map
+// cityData.forEach(function(city){
+//     console.log(city)
+//     L.marker(city.location)
+//     .bindPopup("<h2>" + city.city + ", " + city.state + "</h2><hr><h3>Population: " + city.population.toLocaleString() + "</h3>")
+//     .addTo(map)
+// });
 
-//  more circles, dependent on the screen's zoom
-L.circleMarker([34.0522, -118.2437], {
-    radius: 300,
-    color: "black",
-    fillColor: "#ffffa1"
-}).addTo(map)
+// above but circle and masured by pop
+cityData.forEach(function(city){
+    console.log(city)
+    L.circleMarker(city.location, {
+        radius: city.population/100000,
+        color: "orange"
+    })
+    .bindPopup("<h2>" + city.city + ", " + city.state + "</h2><hr><h3>Population: " + city.population.toLocaleString() + "</h3>")
+    .addTo(map)
+});
 
-// We create the tile layer that will be the background of our map.
+// //  add a circle marker
+// L.circle([34.0522, -118.2437], {
+//     radius: 100
+//  }).addTo(map);
+
+// //  more circles, dependent on the screen's zoom
+// L.circleMarker([34.0522, -118.2437], {
+//     radius: 300,
+//     color: "black",
+//     fillColor: "#ffffa1"
+// }).addTo(map)
+
+// We create the tile layer that will be the background of our map. This goes last
 let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/dark-v10/tiles/{z}/{x}/{y}?access_token={accessToken}', {
     attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
     maxzoom: 18,
